159bfd3aa500b23fd4eb94f2670d7d2f4f5dcaf4
gdevgenich <gdevgenich@gmail.com>
2026-02-24 09:23:40 +0400

    Работа с авторизацией в Playwright - Практика работы с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,36 +1,40 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
-# Открываем браузер с использованием Playwright
 with sync_playwright() as playwright:
-    # Запускаем Chromium браузер в обычном режиме (не headless)
     browser = playwright.chromium.launch(headless=False)
-    # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
     context = browser.new_context()
-    # Открываем новую страницу в рамках контекста
     page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
-
     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
     username_input.fill('username')
-
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
     password_input.fill('password')
-
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
     # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path="storage_state.json")
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json") # Указываем файл с сохраненным состоянием
+    context = browser.new_context(storage_state="storage_state.json")
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    page.wait_for_timeout(5000)
+    toolbar_title = page.get_by_test_id("courses-list-toolbar-title-text")
+    expect(toolbar_title).to_have_text("Courses")
+
+    no_results_text = page.get_by_test_id("courses-list-empty-view-title-text")
+    expect(no_results_text).to_have_text("There is no results")
+
+    empty_list_icon = page.get_by_test_id("courses-list-empty-view-icon")
+    expect(empty_list_icon).to_be_visible()
+
+    empty_description = page.get_by_test_id("courses-list-empty-view-description-text")
+    expect(empty_description).to_have_text("Results from the load test pipeline will be displayed here")
+

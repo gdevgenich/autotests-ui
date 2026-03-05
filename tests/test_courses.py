725c7f033fa0626281dec182048f685d3a8bf88f
gdevgenich <gdevgenich@gmail.com>
2026-03-05 09:38:56 +0400

    Фикстуры Pytest - Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -4,43 +4,21 @@ from playwright.sync_api import sync_playwright, expect
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
+def test_empty_courses_list(chromium_page_with_state):
 
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+    page = chromium_page_with_state
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
+    toolbar_title = page.get_by_test_id("courses-list-toolbar-title-text")
+    expect(toolbar_title).to_have_text("Courses")
 
-        # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
-        context.storage_state(path="storage_state.json")
+    no_results_text = page.get_by_test_id("courses-list-empty-view-title-text")
+    expect(no_results_text).to_have_text("There is no results")
 
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="storage_state.json")
-        page = context.new_page()
+    empty_list_icon = page.get_by_test_id("courses-list-empty-view-icon")
+    expect(empty_list_icon).to_be_visible()
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-        toolbar_title = page.get_by_test_id("courses-list-toolbar-title-text")
-        expect(toolbar_title).to_have_text("Courses")
-
-        no_results_text = page.get_by_test_id("courses-list-empty-view-title-text")
-        expect(no_results_text).to_have_text("There is no results")
-
-        empty_list_icon = page.get_by_test_id("courses-list-empty-view-icon")
-        expect(empty_list_icon).to_be_visible()
-
-        empty_description = page.get_by_test_id("courses-list-empty-view-description-text")
-        expect(empty_description).to_have_text("Results from the load test pipeline will be displayed here")
+    empty_description = page.get_by_test_id("courses-list-empty-view-description-text")
+    expect(empty_description).to_have_text("Results from the load test pipeline will be displayed here")
 

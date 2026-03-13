8dc3b51bc9ed28ef8fbfb6dfc45cf053df361004
gdevgenich <gdevgenich@gmail.com>
2026-03-13 07:57:31 +0400

    Практикуемся в реализации PageObject - Практикуемся в реализации PageObject

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,24 +1,29 @@
-import pytest
-from playwright.sync_api import sync_playwright, expect
+from pages.create_course_page import CreateCoursePage
+from pages.courses_list_page import CoursesListPage
 
 
-@pytest.mark.courses
-@pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
+def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+    # Check elements on page before any actions will be executed
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0",
+                                                        min_score='0')
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
 
-    page = chromium_page_with_state
+    # Fill course fields
+    create_course_page.upload_preview_image("./testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.fill_create_course_form(title="Playwrite", estimated_time="2 weeks", description="Playwrite",
+                                               max_score="100", min_score="10")
+    create_course_page.click_create_course_button()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-    toolbar_title = page.get_by_test_id("courses-list-toolbar-title-text")
-    expect(toolbar_title).to_have_text("Courses")
-
-    no_results_text = page.get_by_test_id("courses-list-empty-view-title-text")
-    expect(no_results_text).to_have_text("There is no results")
-
-    empty_list_icon = page.get_by_test_id("courses-list-empty-view-icon")
-    expect(empty_list_icon).to_be_visible()
-
-    empty_description = page.get_by_test_id("courses-list-empty-view-description-text")
-    expect(empty_description).to_have_text("Results from the load test pipeline will be displayed here")
-
+    # Check values on course list page
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(index=0, title="Playwrite", estimated_time="2 weeks",
+                                                max_score="100", min_score="10")

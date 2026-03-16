e9fe06868d82fabd1553956643738a672f1deba6
gdevgenich <gdevgenich@gmail.com>
2026-03-16 11:58:56 +0400

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -2,13 +2,13 @@ from pages.create_course_page import CreateCoursePage
 from pages.courses_list_page import CoursesListPage
 import pytest
 
-def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+
+def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
-    # Check elements on page before any actions will be executed
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+
+    # Остальной код без изменений
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+
     create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0",
                                                         min_score='0')
     create_course_page.check_visible_exercises_title()
@@ -16,8 +16,8 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
     create_course_page.check_visible_exercises_empty_view()
 
     # Fill course fields
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(title="Playwrite", estimated_time="2 weeks", description="Playwrite",
                                                max_score="100", min_score="10")
     create_course_page.click_create_course_button()

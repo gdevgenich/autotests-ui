a9aa38fd1a6909fbebd8da97990d5f045260557b
gdevgenich <gdevgenich@gmail.com>
2026-03-21 18:55:56 +0400

    Практикуемся в реализации PageComponent - Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -3,24 +3,26 @@ from pages.courses_list_page import CoursesListPage
 import pytest
 
 
+@pytest.mark.courses
+@pytest.mark.regression
 def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
     # Остальной код без изменений
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
 
-    create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0",
+    create_course_page.create_course_form.check_visible(title="", description_text="", estimated_time="", max_score="0",
                                                         min_score='0')
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
-    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.create_course_exercises_toolbar.check_visible()
+    create_course_page.exercises_empty_view.check_visible(title="There is no exercises",
+                                               description='Click on "Create exercise" button to create new exercise')
 
     # Fill course fields
     create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_course_page.fill_create_course_form(title="Playwrite", estimated_time="2 weeks", description="Playwrite",
+    create_course_page.create_course_form.fill(title="Playwrite", estimated_time="2 weeks", description_text="Playwrite",
                                                max_score="100", min_score="10")
-    create_course_page.click_create_course_button()
+    create_course_page.create_course_toolbar.click_create_course_button()
 
     # Check values on course list page
     courses_list_page.toolbar_view.check_visible()
@@ -37,4 +39,5 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.sidebar.check_visible()
 
     courses_list_page.toolbar_view.check_visible()
-    courses_list_page.check_visible_empty_view()
+    courses_list_page.empty_view.check_visible(title="There is no results",
+                                               description="Results from the load test pipeline will be displayed here")
--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -8,8 +8,8 @@ def test_dashboard_displaying(dashboard_page_with_state: DashboardPage):
     dashboard_page_with_state.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
     # Добавили проверку Navbar компонента на странице Dashboard
     dashboard_page_with_state.sidebar.check_visible()
-    dashboard_page_with_state.check_visible_dashboard_title()
-    dashboard_page_with_state.check_visible_scores_chart()
-    dashboard_page_with_state.check_visible_courses_chart()
-    dashboard_page_with_state.check_visible_students_chart()
-    dashboard_page_with_state.check_visible_activities_chart()
+    dashboard_page_with_state.dashboard_toolbar.check_visible()
+    dashboard_page_with_state.scores_chart.check_visible(title="Scores")
+    dashboard_page_with_state.courses_chart.check_visible(title="Courses")
+    dashboard_page_with_state.students_chart.check_visible(title="Students")
+    dashboard_page_with_state.activities_chart.check_visible(title="Activities")

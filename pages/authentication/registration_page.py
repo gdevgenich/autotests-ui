fc3c0727584f72cdc741440ccfa206f80234573e
gdevgenich <gdevgenich@gmail.com>
2026-03-24 19:01:34 +0400

    Наращивание тестовой базы и рефакторинг

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -1,3 +1,4 @@
+import re
 from playwright.sync_api import Page
 from pages.base_page import BasePage
 from components.authentication.registration_form_component import RegistrationFormComponent
@@ -17,3 +18,4 @@ class RegistrationPage(BasePage):
 
     def click_login_link(self):
         self.login_link.click()
+        self.check_current_url(re.compile(".*/#/auth/login"))

a79c5aa1442518e87e2173835a6cc2e21ef63501
gdevgenich <gdevgenich@gmail.com>
2026-04-15 08:51:47 +0400

    Добавляем информацию о переменных окружения в Allure отчет

--- config.py
+++ config.py
@@ -2,10 +2,8 @@
 from enum import Enum
 from typing import Self
 
-from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath, Field, BaseModel
+from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-from tests.pytest.test_fixtures import settings
 
 
 class Browser(str, Enum):
@@ -14,13 +12,17 @@ class Browser(str, Enum):
     CHROMIUM = "chromium"
 
 
-class TestUser(BaseModel):
+class TestUser(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="TEST_USER")
+
     email: EmailStr
     username: str
     password: str
 
 
-class TestData(BaseModel):
+class TestData(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="TEST_DATA")
+
     image_png_file: FilePath
 
 
@@ -38,32 +40,30 @@ class Settings(BaseSettings):
     test_data: TestData
     videos_dir: DirectoryPath
     tracing_dir: DirectoryPath
+    allure_results_dir: DirectoryPath  # Добавили новое поле
     browser_state_file: FilePath
-
-    # Добавили метод initialize
-    @classmethod
-    def initialize(cls) -> Self:  # Возвращает экземпляр класса Settings
-        # Указываем пути
-        videos_dir = DirectoryPath("./videos")
-        tracing_dir = DirectoryPath("./tracing")
-        browser_state_file = FilePath("browser-state.json")
-
-        # Создаем директории, если они не существуют
-        videos_dir.mkdir(exist_ok=True)  # Если директория существует, то игнорируем ошибку
-        tracing_dir.mkdir(exist_ok=True)
-        # Создаем файл состояния браузера, если его нет
-        browser_state_file.touch(exist_ok=True)  # Если файл существует, то игнорируем ошибку
-
-        # Возвращаем модель с инициализированными значениями
-        return Settings(
-            videos_dir=videos_dir,
-            tracing_dir=tracing_dir,
-            browser_state_file=browser_state_file
-        )
 
     def get_base_url(self) -> str:
         return f"{self.app_url}/"
 
+    @classmethod
+    def initialize(cls) -> Self:
+        videos_dir = DirectoryPath("./videos")
+        tracing_dir = DirectoryPath("./tracing")
+        allure_results_dir = DirectoryPath("./allure-results")  # Создаем объект пути к папке
+        browser_state_file = FilePath("browser-state.json")
 
-# Теперь вызываем метод initialize
+        videos_dir.mkdir(exist_ok=True)
+        tracing_dir.mkdir(exist_ok=True)
+        allure_results_dir.mkdir(exist_ok=True)  # Создаем папку allure-results, если она не существует
+        browser_state_file.touch(exist_ok=True)
+
+        return Settings(
+            videos_dir=videos_dir,
+            tracing_dir=tracing_dir,
+            allure_results_dir=allure_results_dir,  # Передаем allure_results_dir в инициализацию настроек
+            browser_state_file=browser_state_file
+        )
+
+
 settings = Settings.initialize()
